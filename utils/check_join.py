649834dda17bcd9610ffd28047204299367ae6b4
Noob Ansh <71966507+sidhu69@users.noreply.github.com>
2026-01-28 20:11:32 +0530

    Update check_join.py

--- utils/check_join.py
+++ utils/check_join.py
@@ -1,15 +1,11 @@
 from aiogram import Bot
-from aiogram.types import ChatMember
+from aiogram.enums import ChatMemberStatus
 from config import CHANNEL_ID
 
 
 async def is_user_joined(bot: Bot, user_id: int) -> bool:
     try:
-        member = await bot.get_chat_member(CHANNEL_ID, user_id)
-        return member.status in [
-            ChatMember.MEMBER,
-            ChatMember.ADMINISTRATOR,
-            ChatMember.OWNER
-        ]
-    except:
+        member = await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=user_id)
+        return member.status != ChatMemberStatus.LEFT
+    except Exception as e:
         return False
