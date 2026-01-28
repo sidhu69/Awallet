216f3a743d5e6eb1a1f8ee4fa850c611beac4748
Noob Ansh <71966507+sidhu69@users.noreply.github.com>
2026-01-28 20:00:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,18 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 
 from config import BOT_TOKEN
 from handlers.start import router
 
 
 async def main():
-    bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+    bot = Bot(
+        token=BOT_TOKEN,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+
     dp = Dispatcher()
     dp.include_router(router)
 
