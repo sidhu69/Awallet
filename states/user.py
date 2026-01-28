fb15aa02f589a22a6e9a776af47bca09501d4b2a
Noob Ansh <71966507+sidhu69@users.noreply.github.com>
2026-01-29 04:08:24 +0530

    Update user.py

--- states/user.py
+++ states/user.py
@@ -1,5 +1,5 @@
-from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.state import StatesGroup, State
 
 
 class UserForm(StatesGroup):
-    waiting_for_name = State()
+    name = State()
