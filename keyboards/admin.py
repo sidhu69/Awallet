aa8cc4e840a4222fa2d32aaaec369f365ce755c5
Noob Ansh <71966507+sidhu69@users.noreply.github.com>
2026-01-29 15:55:53 +0530

    Update admin.py

--- keyboards/admin.py
+++ keyboards/admin.py
@@ -1,16 +1,17 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-def approve_decline_kb(user_id: int):
+
+def approve_decline_kb(user_id: int, amount: int):
     return InlineKeyboardMarkup(
         inline_keyboard=[
             [
                 InlineKeyboardButton(
                     text="✅ Approve",
-                    callback_data=f"approve_{user_id}"
+                    callback_data=f"approve_{user_id}_{amount}"
                 ),
                 InlineKeyboardButton(
                     text="❌ Decline",
-                    callback_data=f"decline_{user_id}"
+                    callback_data=f"decline_{user_id}_{amount}"
                 )
             ]
         ]
