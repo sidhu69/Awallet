a0c8b2e711f8f8d2321692ebaaa7e1e26c0d512b
Noob Ansh <71966507+sidhu69@users.noreply.github.com>
2026-01-29 14:30:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,6 @@ BOT_TOKEN = "8293897408:AAH2yfWvwagpTmdU1EqvKf_l0qEY53ACVvI"
 
 CHANNEL_ID = "@theawalletchannel"  
 # example: "@my_channel"
+OWNER_ID = 8032922682
+OWNER_UPI = "ansh@upi"
 INSTRUCTION_VOICE_MESSAGE_ID = 110
