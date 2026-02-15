2a3ecd6ca3eea5a63776b99e494bfaec9dc82f77
Noob Ansh <71966507+sidhu69@users.noreply.github.com>
2026-02-15 18:22:11 +0530

    Update user.py

--- states/user.py
+++ states/user.py
@@ -1,7 +1,7 @@
 from aiogram.fsm.state import StatesGroup, State
 
 class UserForm(StatesGroup):
-    name = State()        # Optional if you want to collect user's name
-    upi = State()         # For registration UPI
-    screenshot = State()  # Waiting for payment screenshot
-    video = State()       # Waiting for user video upload
+    name = State()
+    upi = State()
+    screenshot = State()
+    video = State()
